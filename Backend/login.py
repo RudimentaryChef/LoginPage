1f4a2b0d9d3e37732cf7143854d2e95fcd1f20fc
Rudimentary_Chef <akrish6@gatech.edu>
2024-06-04 14:54:53 -0400

    added error handling to log in page

--- Backend/login.py
+++ Backend/login.py
@@ -18,12 +18,39 @@ def login():
     #If the request method is post then
     if request.method == "POST":
         #gets the username from the request
+        username, password, errorMessage = get_user_password_from_form()
+        if errorMessage:
+            return jsonify({"message": "Bad Request: {}".format(errorMessage)}), 400
+    #Checks username and password against the "database"
+        return check_user_password(username, password)
+    else:
+        return jsonify({"message": "request method must me POST. It was {}".format(request.method)}), 400
+
+def get_user_password_from_form():
+    """
+    Get username and password from the request form.
+    """
+    success = False
+    try:
+
         username = request.form.get("username")
         password = request.form.get("password")
-    #Checks username and password against the "database"
+        if username == None or password == None:
+            raise Exception("Username or password not provided in the request form.")
+        return username, password, None
+    except Exception as e:
+        print(e)
+        username = request.form.get("username")
+        password = request.form.get("password")
+        return username, password, e
+
+def check_user_password(username, password):
     if username in users and check_password_hash(users[username], password):
         return jsonify({"message": "Welcome {} Login successful".format(username)}), 200
     else:
+        print("hi")
+        print(username)
+        print(password)
         return jsonify({"message": "Invalid username or password"}), 401
 @app.route("/register", methods=["POST"])
 def register():
@@ -32,13 +59,18 @@ def register():
     :return: Json Message, Status code
     """
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        if username in users:
-            return jsonify({"message": "Username already exists. Please choose a different username."}), 409
-        else:
-            users[username] = generate_password_hash(password)
-            return jsonify({"message": "Registered user sucessfully!"}), 201
+        username, password, errorMessage = get_user_password_from_form()
+        if(errorMessage):
+            return jsonify({"message": "Bad Request: {}".format(errorMessage)}), 400
+        return registration_validator(username,password)
+    else:
+        return jsonify({"message": "request method must me POST. It was {}".format(request.method)}), 400
+def registration_validator(username, password):
+    if username in users:
+        return jsonify({"message": "Username already exists. Please choose a different username."}), 409
+    else:
+        users[username] = generate_password_hash(password)
+        return jsonify({"message": "Registered user sucessfully!"}), 201
 if __name__ == "__main__":
     #Note: Debug mode is True while developing. Need to turn it off in production.
     app.run(debug=True)
